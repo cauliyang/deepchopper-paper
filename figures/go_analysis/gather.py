31548408a368d3582827d9a4161ac13c25d88336
Yangyang Li <yangyang.li@northwestern.edu>
2024-10-16 12:09:40 -0500

    refactor: Update gather function to filter and reindex columns

--- figures/go_analysis/gather.py
+++ figures/go_analysis/gather.py
@@ -1,18 +1,36 @@
 import pandas as pd
 
 
-def main():
-    samples = ["HCT116", "A549", "HepG2"]
+def gather(samples: list[str]) -> pd.DataFrame:
     datasets = []
 
     for sample in samples:
         df = pd.read_table(f"./{sample}.txt")
-        df = df.loc[:, ["Term", "PValue"]]
+        df = df.loc[:, ["Term", "PValue", "Genes"]]
         df["Sample"] = sample
         datasets.append(df)
 
     df = pd.concat(datasets)
-    print(df.to_latex(index=False))
+    df = df.query("PValue < 0.05")
+    df = df.reindex(
+        columns=[
+            "Sample",
+            "Term",
+            "Genes",
+            "PValue",
+        ]
+    )
+
+    return df
+
+
+def main():
+    samples = ["HCT116", "A549", "HepG2"]
+    df = gather(samples)
+    styler = df.style
+
+    latex = styler.to_latex()
+    print(latex)
 
 
 if __name__ == "__main__":
